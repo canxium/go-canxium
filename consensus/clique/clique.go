6a1fffddd73947a6dad259fbb4064ef7ba9f4b46
Canximan <canximan@canxium.org>
2023-07-20 20:08:24 +0700

    Move offline mining difficulty and reward to genesis (#22)

--- consensus/clique/clique.go
+++ consensus/clique/clique.go
@@ -460,14 +460,14 @@ func (c *Clique) VerifyUncles(chain consensus.ChainReader, block *types.Block) e
 // VerifyTxSeal checks whether a offline mining transaction satisfies the PoW difficulty requirements,
 // either using the usual ethash cache for it, or alternatively using a full DAG
 // to make remote mining fast.
-func (c *Clique) VerifyTxSeal(tx *types.Transaction, fulldag bool) error {
+func (c *Clique) VerifyTxSeal(config *params.ChainConfig, tx *types.Transaction, fulldag bool) error {
 	return nil
 }
 
 // VerifyTxsSeal checks whether offline mining transactions satisfies the PoW difficulty requirements,
 // either using the usual ethash cache for it, or alternatively using a full DAG
 // to make remote mining fast.
-func (c *Clique) VerifyTxsSeal(txs types.Transactions, fulldag bool) <-chan error {
+func (c *Clique) VerifyTxsSeal(config *params.ChainConfig, txs types.Transactions, fulldag bool) <-chan error {
 	return nil
 }
 
