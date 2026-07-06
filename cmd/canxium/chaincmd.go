9fdc7c5a3372acb75699962a0f4289265b5d10cf
Canximan <neo@canxium.org>
2026-07-07 06:40:52 +0700

    fix the database corrupt while dump account

--- cmd/canxium/chaincmd.go
+++ cmd/canxium/chaincmd.go
@@ -373,6 +373,7 @@ func importPreimages(ctx *cli.Context) error {
 	defer stack.Close()
 
 	db := utils.MakeChainDatabase(ctx, stack, false)
+	defer db.Close()
 	start := time.Now()
 
 	if err := utils.ImportPreimages(db, ctx.Args().First()); err != nil {
@@ -403,7 +404,6 @@ func exportPreimages(ctx *cli.Context) error {
 
 func parseDumpConfig(ctx *cli.Context, stack *node.Node) (*state.DumpConfig, ethdb.Database, common.Hash, error) {
 	db := utils.MakeChainDatabase(ctx, stack, true)
-	defer db.Close()
 	var header *types.Header
 	if ctx.NArg() > 1 {
 		return nil, nil, common.Hash{}, fmt.Errorf("expected 1 argument (number or hash), got %d", ctx.NArg())
@@ -468,7 +468,6 @@ func dump(ctx *cli.Context) error {
 	if err != nil {
 		return err
 	}
-	defer db.Close()
 	config := &trie.Config{
 		Preimages: true, // always enable preimage lookup
 	}
--- cmd/canxium/snapshot.go
+++ cmd/canxium/snapshot.go
@@ -492,7 +492,6 @@ func dumpState(ctx *cli.Context) error {
 	if err != nil {
 		return err
 	}
-	defer db.Close()
 	snapConfig := snapshot.Config{
 		CacheSize:  256,
 		Recovery:   false,
